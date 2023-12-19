ece0f59efc64f2b17376266b63a42ae69e3cf208
Nikita <nkovalev422@gmail.com>
2023-12-19 19:13:56 +0300

    Table View withouterror

--- Navigation/Resourses/ViewControllers/Profile/Extencions to UITableView.swift
+++ Navigation/Resourses/ViewControllers/Profile/Extencions to UITableView.swift
@@ -1,8 +1,19 @@
-//
-//  Extencions to UITableView.swift
-//  Navigation
-//
-//  Created by Ковалев Никита on 15.12.2023.
-//
 
-import Foundation
+import UIKit
+
+extension UITableView {
+
+func setAndLayout(headerView: UIView){
+    tableHeaderView = headerView
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+        headerView.widthAnchor.constraint(equalTo: widthAnchor)
+    ])
+    
+    headerView.setNeedsLayout()
+    headerView.layoutIfNeeded()
+    headerView.frame.size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+}
+
+}
--- Navigation/Resourses/ViewControllers/Profile/ProfileViewController.swift
+++ Navigation/Resourses/ViewControllers/Profile/ProfileViewController.swift
@@ -1,45 +1,122 @@
 import UIKit
 
+
 class ProfileViewController: UIViewController {
+        
+    fileprivate let post = UserPost.userPost()
     
-    let profileHeaderView = ProfileHeaderView()
+    //MARK: - TableView Init
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView.init(
+            frame: .zero , style: .plain    
+        )
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    private enum CellReuseID: String {
+        case post = "PostTableViewCell_ReuseID"
+        case header = "HeaderTableViewCell_ReuseID"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewController()
+        addSubViews()
+        tableViewLayout()
+        tuneTableView()
+        
     }
     
-    
-    
-    // MARK: - Layout
-    
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        view.addSubview(profileHeaderView)
-        profileHeaderView.frame = view.frame
-    }
     
     // MARK: - Setup
     
     private func setupViewController () {
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .systemGray6
+        navigationItem.title = "ProfileUsingTableView"
         title = "Profile"
     }
     
-    // MARK: - ProfileHeaderViewLayout
-    private func headerViewLayout() {
-        let guide = self.view.safeAreaLayoutGuide
+    private func addSubViews() {
+        view.addSubview(tableView)
+    }
+    
+    // MARK: - TableViewLayout
+    
+    private func tableViewLayout() {
+        
+        let safeArea = view.safeAreaLayoutGuide
+                
+        view.addSubview(tableView)
         NSLayoutConstraint.activate([
-            profileHeaderView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            profileHeaderView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            profileHeaderView.topAnchor.constraint(equalTo: guide.topAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
             
         ])
     }
+    
+    // MARK: - TuneTableView
+    
+    private func tuneTableView() {
 
+        tableView.tableFooterView = UIView()
+                
+        tableView.register(PostCell.self, forCellReuseIdentifier: CellReuseID.post.rawValue)
+        tableView.register(ProfileHeaderView.self, forHeaderFooterViewReuseIdentifier: CellReuseID.header.rawValue)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
 }
+    //MARK: - Extensions
+     
+     extension ProfileViewController: UITableViewDataSource {
+         
+         //MARK: - CustomHeader
+         
+         func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+             if section == 0{
+                 guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: CellReuseID.header.rawValue) as? ProfileHeaderView else{fatalError("header failed")}
+                 
+                 return headerView
+             }
+             return nil
+         }
+         
+         
+         func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+             return 210
+         }
+         
+         
+         //MARK: - CustomCell
+         
+         func numberOfSections(in tableView: UITableView) -> Int {
+             1
+             
+         }
+         
+         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+             post.count
+         }
+         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+             guard let cell = tableView.dequeueReusableCell(withIdentifier: CellReuseID.post.rawValue , for: indexPath) as? PostCell
+             else{return UITableViewCell()}
+             
+             cell.configure(userPostInfo: post[indexPath.row])
+             
+             return cell
+         }
+     }
+
+     extension ProfileViewController: UITableViewDelegate{
+     }
+
 
 
 
